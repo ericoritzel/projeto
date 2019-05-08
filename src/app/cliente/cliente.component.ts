92a8bca2a5a3b011e4590a68a962293d6c7564c8
CLEVERSON AVELINO Ferreira <cleversonavelino@gmail.com>
2019-05-07 21:50:21 -0300

    incluindo componentes

---
+++ src/app/cliente/cliente.component.ts
@@ -0,0 +1,19 @@
+import { OnInit, Component } from '@angular/core';
+import { Cliente } from './cliente.model';
+
+@Component({
+    selector: 'cliente',
+    templateUrl: './cliente.component.html',
+    styleUrls: ['./cliente.component.css']
+})
+
+export class ClienteComponent implements OnInit {
+    
+    cliente: Cliente;
+
+    ngOnInit(): void {
+        alert('Olá Mundo!!!');
+    }
+
+
+}
